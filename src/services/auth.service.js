7d406a58e6617080c501b438dcb0a2387f2aa290
Михаил Егоров <morrowi@ya.ru>
2022-03-21 23:17:41 +0300

    21.08.2 23:16

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'http://panel.kdm1.biz/api/auth/';
 
 class AuthService {
   login(user) {
     return axios
       .post(API_URL + 'signin', {
-        username: user.username,
+        email: user.email,
         password: user.password
       })
       .then(response => {
@@ -24,9 +24,15 @@ class AuthService {
 
   register(user) {
     return axios.post(API_URL + 'signup', {
-      username: user.username,
+      //username: user.username,
       email: user.email,
       password: user.password
+    })
+    .then(response => {
+      if (response.data.accessToken) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+      }
+      return response;
     });
   }
 }
